980b24c498796ec1c05e81a1cb9846c312e771d7
Osinachi Aluche <osinachialuchevictor@gmail.com>
2025-09-03 23:24:33 +0100

    Added inline styles to the Navbar code

--- my-company/src/components/Footer.jsx
+++ my-company/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer style={{ background: "#333", color: "white", textAlign: "center", padding: "10px", marginTop: "20px" }}>
+    <footer style={{ background: "Navy", color: "white", textAlign: "center", padding: "10px", marginTop: "20px" }}>
       <p>© 2023 My Company</p>
     </footer>
   );
--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ background: "#333", padding: "10px" }}>
+    <nav style={{ backgroundColor: "Navy", display: "flex", justifyContent: "center", padding: "10px" }}>
       <Link to="/" style={{ color: "white", margin: "10px" }}>Home</Link>
       <Link to="/about" style={{ color: "white", margin: "10px" }}>About</Link>
       <Link to="/services" style={{ color: "white", margin: "10px" }}>Services</Link>

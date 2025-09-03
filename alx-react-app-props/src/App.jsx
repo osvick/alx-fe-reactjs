527d9a9980a962f259e0426cbede20fead30dac0
Osinachi Aluche <osinachialuchevictor@gmail.com>
2025-09-03 23:48:32 +0100

    Updated the necessary folder path

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
-import ProfilePage from './ProfilePage';
-import UserContext from './Usercontext';
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/Usercontext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };

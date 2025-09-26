6dfec2ee562198465668998db32e48a225b8d286
Osinachi Aluche <osinachialuchevictor@gmail.com>
2025-09-26 17:43:35 +0100

    Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,40 @@
 function UserProfile() {
     return (
-        <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-            <img src="https://via.placeholder.com/150 rounded-full w-36 h-36 mx-auto" alt="User" />
-            <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-            <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code  and explore new technologies.</p>
+        <div className="user-profile 
+                bg-gray-100 mx-auto my-10 rounded-lg shadow-lg text-center 
+                   
+                /* --- Small Screen (sm:640px and above --- */
+                max-w-xs p-4
+
+                /* --- Medium Screen (md: 768px and above) --- */*
+                md:max-w-sm md:my-20
+                ">
+
+            <img 
+                src="https://via.placeholder.com/150" 
+                alt="User Profile" 
+                className="
+                rounded-full mx-auto
+                w-24 h-24
+                md:w-36 md:h-36
+                " 
+            />
+            
+            <h1 className="
+                font-bold text-blue-800 my-4 text-center
+                text-lg
+                md:text-xl
+                ">
+                 John Doe
+                </h1> 
+            
+            <p className="
+            text-gray-600 text-center
+            text-sm
+            md:text-base
+            ">
+                Developer at Example Co. Loves to write code and explore new technologies.
+            </p>
         </div>
     );
 }

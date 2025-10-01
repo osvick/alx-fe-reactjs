b0a9dbd84932ba1f0f6a67cb2dc0d9efcb1ee58c
Osinachi Aluche <osinachialuchevictor@gmail.com>
2025-10-01 12:18:26 +0100

    Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

---
+++ recipe-sharing-platform/src/components/MyPhone.jsx
@@ -0,0 +1,11 @@
+function MyPhone() {
+    return(
+        <div className="text-blue-500">
+            <h2>
+                Happy Independence Day 
+            </h2>
+        </div>
+    );
+}
+
+export default MyPhone;

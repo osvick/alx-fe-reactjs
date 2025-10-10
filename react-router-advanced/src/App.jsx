2725efeeaf9565e5c2017994ab3f7299b5925b61
Osinachi Aluche <osinachialuchevictor@gmail.com>
2025-10-11 00:29:49 +0100

    Changed the browser router from Main.jsx to App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
@@ -11,43 +16,42 @@ import ProfileSettings from "./pages/ProfileSettings.jsx";
 import PostPage from "./pages/PostPage.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import { AuthProvider } from "./context/AuthProvider.jsx";
 
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <div style={{ padding: 20 }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-
-          {/* Protected + Dynamic + Nested Profile route */}
-          <Route
-            path="/profile/:username"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          >
-            <Route path="details" element={<ProfileDetails />} />
-            <Route path="settings" element={<ProfileSettings />} />
-          </Route>
-
-          <Route path="/posts/:postId" element={<PostPage />} />
-
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </>
+    <BrowserRouter>
+      <AuthProvider>
+        <Navbar />
+        <div style={{ padding: 20 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/profile/:username"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            >
+              <Route path="details" element={<ProfileDetails />} />
+              <Route path="settings" element={<ProfileSettings />} />
+            </Route>
+            <Route path="/posts/:postId" element={<PostPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
